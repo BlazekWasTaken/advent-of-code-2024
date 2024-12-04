ef7fbfdf4d45837d629b1c4bb6fa21eb09bb8592
Błażej Dymek <dymekblazej@outlook.com>
2024-12-05 00:11:26 +0100

    Days 1 and 2

---
+++ Day1/Day1.cs
@@ -0,0 +1,48 @@
+﻿namespace Day1;
+
+public class Day1
+{
+    private readonly List<int> _list1 = [];
+    private readonly List<int> _list2 = [];
+    
+    public Day1()
+    {
+        var file = File.ReadAllLines("lists.txt");
+        foreach (var line in file)
+        {
+            var split = line.Split(" ");
+            try
+            {
+                _list1.Add(int.Parse(split.First()));
+                _list2.Add(int.Parse(split.Last()));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+    
+    public int Part1()
+    {
+        var distances = new List<int>();
+        var list1 = _list1.ToList();
+        var list2 = _list2.ToList();
+
+        while (list1.Count > 0 && list2.Count > 0)
+        {
+            var smallest1 = list1.Min();
+            var smallest2 = list2.Min();
+            distances.Add(Math.Abs(smallest1 - smallest2));
+            list1.Remove(smallest1);
+            list2.Remove(smallest2);
+        }
+        
+        return distances.Sum(x => x);
+    }
+    
+    public int Part2()
+    {
+        return _list1.Sum(number => number * _list2.FindAll(x => x == number).Count);
+    }
+}
---
+++ Day2/Day2.cs
@@ -0,0 +1,56 @@
+﻿namespace Day2;
+
+public class Day2
+{
+    private List<List<int>> _list = [];
+    
+    public Day2()
+    {
+        var file = File.ReadAllLines("reports.txt");
+        foreach (var line in file)
+        {
+            var split = line.Split(" ");
+            try
+            {
+                _list.Add(split.Select(int.Parse).ToList());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+    
+    public int Part1()
+    {
+        return _list.Where(row => row[0] != row[1]).Count(IsRowCorrect);
+    }
+
+    public int Part2()
+    {
+        return _list.Count(row =>
+        {
+            if (IsRowCorrect(row)) return true;
+            var i = 0;
+            while (i != row.Count)
+            {
+                var newRow = row.ToList();
+                newRow.RemoveAt(i);
+                if (IsRowCorrect(newRow)) return true;
+                i++;
+            }
+            return false;
+        });
+    }
+
+    private static bool IsRowCorrect(List<int> row)
+    {
+        var isIncreasing = row[0] <= row[1] ? 1 : -1;
+        for (var i = 0; i < row.Count - 1; i++)
+        {
+            var diff = (row[i + 1] - row[i]) * isIncreasing;
+            if (diff is < 1 or > 3) return false;
+        }
+        return true;
+    }
+}
