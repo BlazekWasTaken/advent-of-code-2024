911c9b69ad0fefde7b513149fabad60b81955cba
Błażej Dymek <dymekblazej@outlook.com>
2024-12-05 12:50:15 +0100

    Day 3

---
+++ Day3/Day3.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day3;
+
+public class Day3
+{
+    private readonly string _memory = File.ReadAllText("memory.txt");
+    
+    public int Part1()
+    {
+        var multiplications = 0;
+        var matches = 
+            Regex.Matches(_memory, @"mul\([0-9]+,[0-9]+\)").Select(m => m.Value).ToList();
+        foreach (var match in matches)
+        {
+            var num1 = int.Parse(match.Split(",")[0].Split("(")[1]);
+            var num2 = int.Parse(match.Split(",")[1].Split(")")[0]);
+            multiplications += num1 * num2;
+        }
+        return multiplications;
+    }
+    
+    public int Part2()
+    {
+        var multiplications = 0;
+        var dos = _memory.Split("do()");
+        foreach (var segment in dos)
+        {
+            var donts = segment.Split("don't()");
+            for (int j = 0; j < donts.Length; j++)
+            {
+                if (j != 0) continue;
+                var matches = 
+                    Regex.Matches(donts[j], @"mul\([0-9]+,[0-9]+\)").Select(m => m.Value).ToList();
+                foreach (var match in matches)
+                {
+                    var num1 = int.Parse(match.Split(",")[0].Split("(")[1]);
+                    var num2 = int.Parse(match.Split(",")[1].Split(")")[0]);
+                    multiplications += num1 * num2;
+                }
+            }
+        }
+        return multiplications;
+    }
+}
--- advent-of-code-2024/Program.cs
+++ advent-of-code-2024/Program.cs
@@ -1,11 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello! This is the Advent of Code 2024!");
-
-var day1 = new Day1.Day1();
+﻿var day1 = new Day1.Day1();
 Console.WriteLine($"Day 1 part 1: {day1.Part1()}");
 Console.WriteLine($"Day 1 part 2: {day1.Part2()}");
 
 var day2 = new Day2.Day2();
 Console.WriteLine($"Day 2 part 1: {day2.Part1()}");
 Console.WriteLine($"Day 2 part 2: {day2.Part2()}");
+
+var day3 = new Day3.Day3();
+Console.WriteLine($"Day 3 part 1: {day3.Part1()}");
+Console.WriteLine($"Day 3 part 2: {day3.Part2()}");
